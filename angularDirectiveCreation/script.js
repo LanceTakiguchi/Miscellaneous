90e73829c79f222aaeb012a27524901d739c4351
ltakiguc <ltakiguc@uci.edu>
2016-10-21 16:34:43 -0700

    Adding in student via controller into the table

--- angularDirectiveCreation/script.js
+++ angularDirectiveCreation/script.js
@@ -13,6 +13,29 @@ app.directive("student", function(){
     restrict: 'AE',
     templateUrl: function(elem, attr){
       return 'student-' + attr.type + '-template.html';
-    }
+    },
+        controller: function(){
+        this.studentArr = [
+          {name: 'Bruce Wayne',
+           course: 'Economics',
+           grade: '94'
+          },
+          {name: 'Clark Kent',
+           course: 'Creative Writing',
+           grade: '81'
+            
+          },
+          {name: 'Hal Jordan',
+           course: 'Avionics',
+           grade: '88'
+            
+          },
+          {name: 'Barry Ale',
+           course: 'Forensic Science',
+           grade: '98'
+          },
+        ];
+    }, 
+    controllerAs: 'sc'
   }
 });
